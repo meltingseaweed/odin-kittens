dca84bcbcd9a847212dd787593080fd724e29088
meltingseaweed <conor.seo@gmail.com>
2026-06-02 12:35:13 +0900

    Finish project

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,63 @@
+class KittensController < ApplicationController
+  def index
+    @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @kittens }
+      format.json { render json: @kittens }
+    end
+  end
+
+  def show
+    find_kitten
+
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @kitten }
+      format.json { render json: @kitten }
+    end
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:notice] = "New family member Meow!"
+      redirect_to root_path
+    else
+      flash[:alert] = "Meow.. Some kind of error occurred.. but I'm a cat so I don't know what it was.."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    find_kitten
+  end
+  def update
+    if @kitten.update!(kitten_params)
+      flash[:notice] = "Kitten updated Meow!"
+      redirect_to @kitten
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    find_kitten
+    @kitten.destroy
+    redirect_to root_path, notice: "Kitten found a happy new home, Meow!"
+  end
+
+  private
+
+  def find_kitten
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def kitten_params
+    params.expect(kitten: [ :name, :age, :cuteness, :softness ])
+  end
+end
